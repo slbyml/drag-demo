cc73d5e3b4aa1e0a710bb64e06a596704fe63174
shaolibao <994917123@qq.com>
2021-03-02 16:45:01 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-})
+// export default defineConfig({
+//   base: "",
+//   plugins: [vue()]
+// })
+export default ({ command, mode }) => {
+  const config = {
+    base: '/',
+    plugins: [vue()],
+  }
+  if (command === 'build') {
+    config.base = '/drag-demo/dist/' 
+  }
+  return config
+}
